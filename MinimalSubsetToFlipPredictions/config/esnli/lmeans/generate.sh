6d4465ae34b5a93e3248312016c2641d3b8b1bcd
SamSoup <sam.su@utexas.edu>
2024-05-23 12:00:25 -0500

    Add config for Lmeans

--- MinimalSubsetToFlipPredictions/config/esnli/lmeans/generate.sh
+++ MinimalSubsetToFlipPredictions/config/esnli/lmeans/generate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+incre=$1
 
 # Base JSON configuration
 base_config='{
@@ -9,7 +10,7 @@ base_config='{
     "layer": 24,
     "wrapper_name": "LMeans",
     "output_dir": "MinimalSubset",
-    "iterative_threshold": 10,
+    "iterative_threshold": 100,
     "splits": 10,
     "idx_start": __START__,
     "idx_end": __END__
@@ -18,13 +19,13 @@ base_config='{
 # Function to generate JSON config file
 generate_config() {
   start=$1
-  end=$((start + 700))
+  end=$((start + $incre))
   filename="deberta_mean_with_attention_LMeans_${start}_${end}.json"
 
   echo "$base_config" | sed -e "s/__START__/${start}/" -e "s/__END__/${end}/" > $filename
 }
 
 # Generate config files
-for ((i = 0; i < 10500; i += 700)); do
+for ((i = 0; i < 9900; i += $incre)); do
   generate_config $i
 done
