073ee8063823777b5bb7573effee9d73731652cd
SamSoup <sam.su@utexas.edu>
2024-05-28 14:11:00 -0500

    add config for lmeans eval

---
+++ MinimalSubsetToFlipPredictions/config/esnli/evaluate_lmeans/generate.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+incre=$1
+num_samples=$2
+
+# Base JSON configuration
+base_config='{
+    "dataset": "esnli",
+    "subsets_filename": "results/esnli/esnli_deberta_large_LMeans.pickle",
+    "model": "deberta_large",
+    "seed": 42,
+    "pooler": "mean_with_attention",
+    "layer": 24,
+    "do_yang2023": true,
+    "algorithm_type": "fast",
+    "output_dir": "MinimalSubset/evaluate/lmeans/",
+    "idx_start": __START__,
+    "idx_end": __END__
+}'
+
+# Function to generate JSON config file
+generate_config() {
+  start=$1
+  end=$((start + $incre))
+  if [ $num_samples -lt $end ]; then
+      end=$num_samples
+  fi
+  filename="${start}_${end}.json"
+
+  echo "$base_config" | sed -e "s/__START__/${start}/" -e "s/__END__/${end}/" > $filename
+}
+
+# Generate config files
+for ((i = 0; i < $num_samples; i += $incre)); do
+  generate_config $i
+done
