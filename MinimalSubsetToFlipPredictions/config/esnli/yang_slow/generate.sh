ee15baf17f66265fb3d3d49127f3cfff5586ef77
SamSoup <sam.su@utexas.edu>
2024-06-06 18:52:45 -0500

    Ready to run yang

--- MinimalSubsetToFlipPredictions/config/esnli/yang_slow/generate.sh
+++ MinimalSubsetToFlipPredictions/config/esnli/yang_slow/generate.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
-incre=$1
+nodes=$1
+total=$2
+# Calculate the ceiling of total / nodes
+if [ $((total % nodes)) -ne 0 ]; then
+  incre=$((total / nodes + 1))
+else
+  incre=$((total / nodes))
+fi
 
 # Base JSON configuration
 base_config='{
     "dataset": "esnli",
-    "model": "deberta-large",
+    "model": "deberta_large",
     "seed": 42,
     "pooler": "mean_with_attention",
     "layer": 24,
+    "thresh": 0.5,
+    "l2": 1,
     "do_yang2023": true,
+    "wrapper_name": "LogisticRegression",
     "algorithm_type": "slow",
-    "output_dir": "MinimalSubset/yang_slow/__START__to__END__",
+    "output_dir": "MinimalSubset/yang_fast/__START__to__END__",
     "idx_start": __START__,
     "idx_end": __END__
 }'
@@ -19,12 +29,12 @@ base_config='{
 generate_config() {
   start=$1
   end=$((start + $incre))
-  filename="deberta_mean_with_attention_yang_slow_${start}_${end}.json"
+  filename="${start}_${end}.json"
 
   echo "$base_config" | sed -e "s/__START__/${start}/" -e "s/__END__/${end}/" > $filename
 }
 
 # Generate config files
-for ((i = 0; i < 9900; i += $incre)); do
+for ((i = 0; i < $total; i += $incre)); do
   generate_config $i
 done
