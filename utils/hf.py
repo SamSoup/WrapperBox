0ba8055e129b225aed97fcf5abab80f26681f4bf
SamSoup <sam.su@utexas.edu>
2024-07-09 16:07:11 -0500

    Test ComputeRepresentations

--- utils/hf.py
+++ utils/hf.py
@@ -1,6 +1,7 @@
 from typing import Tuple
 from transformers import AutoTokenizer, AutoModel
 from utils.constants.directory import CACHE_DIR
+import torch
 
 
 def get_model_and_tokenizer(model_name: str) -> Tuple[AutoModel, AutoTokenizer]:
@@ -16,4 +17,9 @@ def get_model_and_tokenizer(model_name: str) -> Tuple[AutoModel, AutoTokenizer]:
     """
     tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=CACHE_DIR)
     model = AutoModel.from_pretrained(model_name, cache_dir=CACHE_DIR)
+
+    if torch.cuda.device_count() > 1:
+        model = torch.nn.DataParallel(model)
+    model.to(torch.device("cuda" if torch.cuda.is_available() else "cpu"))
+
     return model, tokenizer
