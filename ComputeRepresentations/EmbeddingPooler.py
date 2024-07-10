4f126ffe9bf799ef175bc3ab54cf70c241399f45
SamSoup <sam.su@utexas.edu>
2024-07-10 14:21:46 -0500

    update more configs

--- ComputeRepresentations/EmbeddingPooler.py
+++ ComputeRepresentations/EmbeddingPooler.py
@@ -2,35 +2,78 @@ from typing import Callable
 import torch
 import torch.nn.functional as F
 
+
 class EmbeddingPooler:
     """
     This class hosts various methods for obtaining a batch of token-level embeddings
     to a sentence level embedding. Notice that using Sentence-Bert directly didn't work.
-    
-    All function take as input an array of dimensions (batch_size, max_seq_len, hidden_dim), 
+
+    All function take as input an array of dimensions (batch_size, max_seq_len, hidden_dim),
     where max_seq_len is dependent upon the examples in the batch (dynamic), and
     hidden_dim is the number of hidden units that's model dependent
-    
+
     Max pooling?
     """
+
     def __init__(self):
         self.name_to_fct = {
             "mean_with_attention": self.mean_with_attention,
             "mean": self.mean,
             "cls": self.cls,
             "flatten": self.flatten,
-            "eos": self.eos
+            "eos": self.eos,
         }
 
     def mean(self, hidden_states: torch.tensor, *_) -> torch.tensor:
         return torch.mean(hidden_states, dim=1).squeeze().detach().cpu()
 
-    def mean_with_attention(self, hidden_states: torch.tensor, attention_mask: torch.tensor) -> torch.tensor:
-        input_mask_expanded = attention_mask.unsqueeze(-1).expand(hidden_states.size()).float()
-        return (torch.sum(hidden_states * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)).squeeze().detach().cpu()
+    def mean_with_attention(
+        self, hidden_states: torch.tensor, attention_mask: torch.tensor
+    ) -> torch.tensor:
+        # NOTE: this attention is actually just the input mask telling
+        # the model to ignore padded tokens for batched inputs
+        input_mask_expanded = (
+            attention_mask.unsqueeze(-1).expand(hidden_states.size()).float()
+        )
+        return (
+            (
+                torch.sum(hidden_states * input_mask_expanded, 1)
+                / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+            )
+            .squeeze()
+            .detach()
+            .cpu()
+        )
+
+    def mean_with_attention_heads(
+        self, hidden_states: torch.tensor, attention: torch.tensor
+    ) -> torch.tensor:
+        """
+        Average over the attention heads/weights per token, and then
+        compute the representation as attention \dot hidden_state
+
+        Args:
+            hidden_states (torch.tensor): (batch_size, seq_length, hidden_size)
+            attention (torch.tensor): (batch_size, num_heads, seq_length, seq_length)
+
+        Returns:
+            torch.tensor: _description_
+        """
+        # Average attention weights across heads
+        att_avg_head = attention.mean(
+            dim=1
+        )  # (batch_size, seq_length, seq_length)
+
+        # Average attention weights across tokens (for simplicity, you might want more sophisticated methods)
+        att_avg_tokens = att_avg_head.mean(dim=-1)  # (batch_size, seq_length)
+
+        # Apply the attention weights to the hidden states
+        return (hidden_states * att_avg_tokens.unsqueeze(-1)).sum(dim=1)
 
     def cls(self, hidden_states: torch.tensor, *_):
-        return hidden_states[:, 0, :].squeeze().detach().cpu() # first token is always cls
+        return (
+            hidden_states[:, 0, :].squeeze().detach().cpu()
+        )  # first token is always cls
 
     def eos(self, hidden_states: torch.tensor, attention_mask: torch.tensor):
         # return the last valid token
