e1b8f103a22d414db7b08a728bc3cfea23b7aafb
SamSoup <sam.su@utexas.edu>
2024-07-22 15:33:51 -0500

    update configs, main to remove datasets import

--- ComputeRepresentations/SentenceTransformers/main.py
+++ ComputeRepresentations/SentenceTransformers/main.py
@@ -8,9 +8,10 @@ import argparse
 import json
 import os
 import numpy as np
-from datasets import load_dataset
+import pandas as pd
+import torch
 from sentence_transformers import SentenceTransformer
-from utils.constants.directory import CACHE_DIR
+from utils.constants.directory import CACHE_DIR, DATA_DIR
 from utils.io import mkdir_if_not_exists
 from torch.utils.data import DataLoader, Dataset
 from tqdm import tqdm
@@ -120,58 +121,69 @@ if __name__ == "__main__":
         "test": args.do_test,
     }
     mkdir_if_not_exists(args.output_dir)
-    datasets = load_dataset(
-        args.dataset_name_or_path, token=True, cache_dir=CACHE_DIR
-    )
 
+    # Load Datasets, from disk because
+    ## datasets might require a GLIBC version higher than what is available
+    datasets = {}
+    for split in to_dos:
+        datasets[split] = pd.read_csv(
+            os.path.join(
+                DATA_DIR, "datasets", args.dataset_name_or_path, f"{split}.csv"
+            )
+        )
+
+    # Load the model, wrapping it for multi-gpu if needed
     model = SentenceTransformer(args.model_name_or_path, cache_folder=CACHE_DIR)
+    if torch.cuda.device_count() > 1:
+        print(f"Using {torch.cuda.device_count()} GPUs!")
+        model = torch.nn.DataParallel(model)
+    model = model.to("cuda")
+    model = model.module  # call on the inner module later
+
     for split, dataset in datasets.items():
-        if to_dos[split]:
-            print(f"***** Computing Representations for {split} dataset *****")
-            # Assume that the column to compute representation is for is 'text'
-            if args.instruction is not None:
-                dataset = InstructionSentenceDataset(
-                    dataset["text"], args.instruction
-                )
-            else:
-                dataset = SentenceDataset(dataset["text"])
+        print(f"***** Computing Representations for {split} dataset *****")
+        # Assume that the column to compute representation is for is 'text'
+        if args.instruction is not None:
+            dataset = InstructionSentenceDataset(
+                dataset["text"], args.instruction
+            )
+        else:
+            dataset = SentenceDataset(dataset["text"])
+        dataloader = DataLoader(
+            dataset, batch_size=args.batch_size, shuffle=False
+        )
+
+        chunk_size = (
+            args.chunk_size if len(dataset) > args.chunk_size else len(dataset)
+        )
+        num_chunks = (len(dataset) // chunk_size) + 1
+
+        for i in range(num_chunks):
+            start_idx = i * chunk_size
+            end_idx = min((i + 1) * chunk_size, len(dataset))
+            dataset_chunk = dataset.select(range(start_idx, end_idx))
+
+            print(
+                f"Processing chunk {i+1}/{num_chunks} with indices {start_idx} to {end_idx}"
+            )
+
             dataloader = DataLoader(
-                dataset, batch_size=args.batch_size, shuffle=False
+                dataset_chunk, batch_size=args.batch_size, shuffle=False
             )
 
-            chunk_size = (
-                args.chunk_size
-                if len(dataset) > args.chunk_size
-                else len(dataset)
+            representations = []
+            for batch in tqdm(dataloader):
+                embeddings = model.encode(batch, convert_to_numpy=True)
+                representations.append(embeddings)
+
+            representations = np.vstack(representations)
+            save_path = os.path.join(
+                args.output_dir,
+                (
+                    f"{split}_chunk_{i+1}.npy"
+                    if len(dataset) > args.chunk_size
+                    else f"{split}.npy"
+                ),
             )
-            num_chunks = (len(dataset) // chunk_size) + 1
-
-            for i in range(num_chunks):
-                start_idx = i * chunk_size
-                end_idx = min((i + 1) * chunk_size, len(dataset))
-                dataset_chunk = dataset.select(range(start_idx, end_idx))
-
-                print(
-                    f"Processing chunk {i+1}/{num_chunks} with indices {start_idx} to {end_idx}"
-                )
-
-                dataloader = DataLoader(
-                    dataset_chunk, batch_size=args.batch_size, shuffle=False
-                )
-
-                representations = []
-                for batch in tqdm(dataloader):
-                    embeddings = model.encode(batch, convert_to_numpy=True)
-                    representations.append(embeddings)
-
-                representations = np.vstack(representations)
-                save_path = os.path.join(
-                    args.output_dir,
-                    (
-                        f"{split}_chunk_{i+1}.npy"
-                        if len(dataset) > args.chunk_size
-                        else f"{split}.npy"
-                    ),
-                )
-                # Save representations as numpy file
-                np.save(save_path, representations)
+            # Save representations as numpy file
+            np.save(save_path, representations)
